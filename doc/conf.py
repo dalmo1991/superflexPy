e4b3daa996a606b6bedf231742e891f6afc3bf42
Marco Dal Molin <marco.dalmolin@eawag.ch>
2019-11-14 15:20:39 +0100

    Update

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Marco Dal Molin'
 author = 'Marco Dal Molin'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.2.0-alpha'
 
 
 # -- General configuration ---------------------------------------------------
