bb97513e52dcd83d674d625c7adfe3c6aba0e103
Marco Dal Molin <marco.dalmolin@eawag.ch>
2019-11-19 09:49:15 +0100

    Copyright changed

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'SuperflexPy'
-copyright = '2019, Marco Dal Molin'
+copyright = '2019, Marco Dal Molin et al.'
 author = 'Marco Dal Molin'
 
 # The full version, including alpha/beta/rc tags
